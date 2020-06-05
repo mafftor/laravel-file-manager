ca56205e452bb19252941a277ebd93ddc4229686
Mafftor <mafftor@gmail.com>
2020-06-05 16:33:37 +0300

    Fixed download attack

--- src/Controllers/DownloadController.php
+++ src/Controllers/DownloadController.php
@@ -2,10 +2,24 @@
 
 namespace Mafftor\LaravelFileManager\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+
 class DownloadController extends LfmController
 {
     public function getDownload()
     {
-        return response()->download($this->lfm->setName(request('file'))->path('absolute'));
+        $filepath = $this->helper->getCategoryName() . request('working_dir') . '/' . request('file');
+
+        try {
+            if (Storage::disk($this->helper->config('disk'))->exists($filepath)) {
+                return response()->download($this->lfm->setName(request('file'))->path('absolute'));
+            }
+        } catch (\Exception $e) {
+            // Do not need to throw the exception
+        }
+
+        Log::error('[laravel-file-manager] File not found - ' . $filepath);
+        abort(404);
     }
 }
